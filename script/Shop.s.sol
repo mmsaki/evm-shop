fceb32bdef7349f10f1a897242d6162175b882ed
Meek Msaki <meek10x@gmail.com>
2025-12-19 21:06:13 -0600

    fix refund order bug

--- script/Shop.s.sol
+++ script/Shop.s.sol
@@ -6,11 +6,11 @@ import {Shop} from "../src/Shop.sol";
 
 contract CounterScript is Script {
     Shop public shop;
-    uint256 immutable PRICE = 1 ** 16;
+    uint256 immutable PRICE = 1e16;
     uint16 immutable TAX = 100;
     uint16 immutable TAX_BASE = 1000;
-    uint16 immutable REFUND_BASE = 500;
-    uint16 immutable REFUND_RATE = 1000;
+    uint16 immutable REFUND_RATE = 500;
+    uint16 immutable REFUND_BASE = 1000;
     uint256 immutable REFUND_POLICY = 24 hours;
 
     function setUp() public {}
